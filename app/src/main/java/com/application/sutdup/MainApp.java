3faaedd842aa45a54071cc279f59490dd7787d13
sufi4414 <mdsufyanali@gmail.com>
2023-02-17 02:16:29 +0800

    Basic bottom nav ui
    Please tell me it works on your comp

--- app/src/main/java/com/application/sutdup/MainApp.java
+++ app/src/main/java/com/application/sutdup/MainApp.java
@@ -3,21 +3,53 @@ package com.application.sutdup;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.application.sutdup.Library.Database;
+import com.application.sutdup.Library.ui.HomeFragment;
+import com.application.sutdup.Library.ui.ProfileFragment;
+import com.application.sutdup.Library.ui.SettingsFragment;
+import com.application.sutdup.R;
+import com.application.sutdup.databinding.ActivityMainBinding;
 
 public class MainApp extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
-        // TODO: To set layout of UI (next line)
-        //setContentView(R.layout.activity_main);
+        binding.bottomNavView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.search:
+                    replaceFragment(new SettingsFragment() );
+                    break;
+            }
+
+            return true;
+        });
 
 
-        Database.main();
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
-
