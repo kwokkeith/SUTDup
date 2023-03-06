9e43113a2eeccc8c42eb93cb2c6c0719206b68be
Kwok Keith <keithkwok-@hotmail.com>
2023-03-06 23:47:42 +0800

    Edited docstrings in Validator.java

--- app/src/main/java/com/application/sutdup/Library/Validator.java
+++ app/src/main/java/com/application/sutdup/Library/Validator.java
@@ -8,34 +8,41 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Validator {
 /**
  * This class contains the validators required for inputs
  * Editors: Kwok Keith
- */
+ **/
+public class Validator {
 
+    /**
+      Check that input `String` contains ONLY letters.
+      @params input: String
+     **/
     public static void isWord(String input) throws Exception {
-        /*
-          Check that input <String> contains ONLY letters.
-         */
+
         if (!Pattern.matches("[a-zA-Z]+", input))
             throw new ValidatorException("isWord", "Input String must contain only letters") ;
     }
 
+    /**
+     Check that input `String` contains letters and/or whitespaces.
+     @params input: String
+     **/
     public static void isSentence(String input) throws Exception {
-        /*
-         * Check that input <String> contains letters and/or whitespaces.
-         */
+
         if(!Pattern.matches("[a-zA-Z\\s]*", input)){
             throw new ValidatorException("isSentence", "Input String must contain only letters" +
                     "or whitespaces") ;
         };
     }
 
+    /**
+     Check that input `String` length is at most atMostLength `Integer`
+        @params input: String
+        @params atMostLength: Int
+     **/
     public static void lengthCheck(String input, int atMostLength) throws Exception {
-        /*
-         *  Check that input <String> length is at most atMostLength <Integer>
-         */
+
         if (atMostLength < 0)
         {
             throw new ValidatorException("lengthCheck", "Input length cannot be less than 0");
@@ -46,10 +53,14 @@ public class Validator {
         };
     }
 
+    /**
+     * Check that input `String` length is at most max `Integer` and at least min `Integer`
+        @params input: String
+        @params min: Int
+        @params max: Int
+     **/
     public static void lengthCheck(String input, int min, int max) throws Exception {
-        /*
-         * Check that input <String> length is at most max <Integer> and at least min <Integer>
-         **/
+
 
         if (min < 0)
         {
@@ -65,10 +76,12 @@ public class Validator {
         }
     }
 
+    /**
+     * Checks if an input is a number
+     * @param input: String
+     **/
     public static void isNumber(String input) throws Exception{
-        /**
-         * Checks if an input is a number
-         **/
+
         try{
             Integer.parseInt(input);
         }
@@ -77,10 +90,12 @@ public class Validator {
         }
     }
 
+    /**
+     * Checks if an input is all digits `Strings``/Strings`
+     * @params input: String
+     **/
     public static void isDigits(String input) throws Exception{
-        /**
-         * Checks if an input is all digits <Strings></Strings>
-         */
+
         for (int i=0; i < input.length(); i++){
             if (!Character.isDigit(input.charAt(i)))
             {
@@ -89,33 +104,41 @@ public class Validator {
         }
     }
 
+    /**
+     * Checks if input string is a valid Singapore phone number
+     * @param input: String
+     **/
     public static void isSGPhoneNumber(String input) throws Exception
     {
-        /**
-         * Checks if input string is a valid Singapore phone number
-         */
+
         if(!(input.length() == 8 && input.matches("[0-9]+"))){
             throw new ValidatorException( "isSGPhoneNumber", "Input String must " +
                     "be of the format (dddd dddd) i.e. 9838 7583.");
         }
     }
 
+    /**
+     * Checks if a number is within the range of min and max (Inclusive)
+     * @params num: Int
+     * @params min: Int (Inclusive)
+     * @params max: Int (Inclusive)
+     **/
     public static void rangeCheck(int num, int min, int max) throws Exception
     {
-        /**
-         * Checks if a number is within the range of min and max (Inclusive)
-         */
+
         if(!(num >= min && num <= max))
             throw new ValidatorException( "rangeCheck", Integer.toString(num) + "is not within " +
                     Integer.toString(min) + " and " + Integer.toString(max));
     }
 
+    /**
+     * Check if the password is at least 8 characters and contains number, capital,
+     * special character and lowercase
+     * @params password: String
+     **/
     public static void isPassword(String password) throws Exception
     {
-        /**
-         * Check if the password is at least 8 characters and contains number, capital,
-         * special character and lowercase
-         */
+
         if (password.length() > 7)
         {
             if(!checkPass(password))
@@ -129,11 +152,13 @@ public class Validator {
 
     }
 
+    /**
+     * Takes in a password string and checks if it contains number, capital and lowercase
+     * letters, and special character.
+     * @params password: String
+     **/
     private static boolean checkPass(String password) throws Exception {
-        /**
-         * Takes in a password string and checks if it contains number, capital and lowercase
-         * letters, and special character.
-         */
+
         boolean containsNum = false;
         boolean containsCap = false;
         boolean containsLow = false;
@@ -159,11 +184,13 @@ public class Validator {
         return false;
     }
 
+    /**
+     * Check if input string has whitespace (Space)
+     * @params input: String
+     **/
     public static void hasSpace(String input) throws Exception
     {
-        /**
-         * Check if input string has whitespace (Space)
-         */
+
        for (char currentChar : input.toCharArray()){
            if (Character.isWhitespace(currentChar)) {
                return;
@@ -172,11 +199,13 @@ public class Validator {
         throw new ValidatorException( "hasSpace", "Input String contains no whitespace");
     }
 
+    /**
+     * Check if input string is a valid ID, contains number and letters only
+     * @params input: String
+     **/
     public static void isValidId(String input) throws Exception
     {
-        /**
-         * Check if input string is a valid ID, contains number and letters only
-         */
+
         if(!Pattern.matches("[a-zA-Z0-9]*", input)){
             throw new ValidatorException("isValidId", "Input String must contain letters" +
                     "and/or numbers only") ;
@@ -184,11 +213,13 @@ public class Validator {
 
     }
 
+    /**
+     * Checks if an input string contains special characters
+     * @params input: String
+     **/
     public static void hasSpecialCharacter(String input) throws Exception
     {
-        /**
-         * Checks if an input string contains special characters
-         */
+
         Pattern sPattern = Pattern.compile("[a-zA-Z0-9]*");
         Matcher sMatcher = sPattern.matcher(input);
 
@@ -198,11 +229,13 @@ public class Validator {
          }
     }
 
+    /**
+     * Checks if an input satisfies an email format. ____@____.com
+     * @params input: String
+     **/
     public static void isEmail(String input) throws Exception
     {
-        /**
-         * Checks if an input satisfies an email format
-         */
+
         String emailRegex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
         Pattern emailPat = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
         Matcher matcher = emailPat.matcher(input);
@@ -211,11 +244,13 @@ public class Validator {
         }
     }
 
+    /**
+     * Checks if the input date is a valid date of dd/mm/yyyy format
+     * @params date: String
+     **/
     public static void isDate(String date) throws Exception
     {
-        /**
-         * Checks if the input date is a valid date of dd/mm/yyyy format
-         */
+
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu",
@@ -235,11 +270,13 @@ public class Validator {
                 ", format should be in dd/mm/yyyy");
     }
 
+    /**
+     * Checks if String input is a valid image file extension
+     * @params input: String
+     **/
     public static void isValidImageFile(String input) throws Exception
     {
-        /*
-        * Checks if String input is a valid image file extension
-         */
+
         Pattern sPattern = Pattern.compile("[^\\\\s]+(\\\\.(?i)(jpe?g|png|gif|bmp))$");
         Matcher sMatcher = sPattern.matcher(input);
         if(!sMatcher.matches()){
@@ -248,31 +285,37 @@ public class Validator {
         }
     }
 
+    /**
+     *   Check if an input string is at most 2 decimal place
+     * @params input: String
+     **/
     public static void isUpToTwoDecimal(String input) throws Exception{
-        /*
-        *   Check if an input string is at most 2 decimal place
-         */
+
         if(!(input.matches("^\\d+\\.\\d{0,2}$"))){
             throw new ValidatorException("isUpToTwoDecimal", "Input String is not up to 2 decimal" +
                     "places. (Exceeds 2 decimal places)");
         }
     }
 
+    /**
+     * Checks if input string is an empty string, throws Exception.
+     * @params input: String
+     **/
     public static void isEmpty(String input) throws Exception
     {
-        /*
-        * Checks if input string is an empty string, throws Exception.
-         */
+
         if (!(input.matches("[^$]"))){
            throw new ValidatorException( "isEmpty", "Input String is not empty");
         }
     }
 
+    /**
+     * Checks if input string is an empty string, returns Boolean
+     * @params input: String
+     **/
     public static boolean isEmptyBool(String input)
     {
-        /*
-         * Checks if input string is an empty string, returns Boolean
-         */
+
         return input.matches("[^$]");
     }
 
